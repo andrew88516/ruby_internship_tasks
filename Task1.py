aefe606c159f0fdb5e52576262c350bd41638932
ThatX <44832716+andrew88516@users.noreply.github.com>
2021-03-05 16:43:11 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -5,11 +5,10 @@ class Order:
         self.discounts = discounts
     
     def total_sum(self):
-        '''I used the list comprehension, then passed the generated list to internal sum() function, since 
-        this way seemed to me more concise than using a "for" loop
+        '''I used the list comprehension, then passed the generated list to internal sum() function, since this way seemed to me more concise than using a "for" loop
         '''
         return sum([x - (y / 100 * x) for x, y in zip(self.items, self.discounts)])
 
 order1 = Order([100.0, 112.2, 301.1], [10.0, 0.0, 0.0])
 
-order1.total_sum()
+print(order1.total_sum())
